5d7d2bb54a17d6668ef0730129b83d3addcb885d
EC2 Default User <ec2-user@ip-172-31-47-60.us-east-2.compute.internal>
2019-06-13 09:25:54 +0000

    Chenge not yaml use json file in flextype 9.0

---
+++ bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+namespace Flextype;
+use Flextype\Component\Filesystem\Filesystem;
+
+
+$flextype->emitter->addListener('onThemeTail', function() {
+    
+    $getInSettings = JsonParser::decode(Filesystem::read(ROOT_DIR . '/site/plugins/data-guard/settings.json')); 
+    
+    echo(yaCode($getInSettings["YandexId"]));
+   
+});
+
+function yaCode(int $id) : string {
+    return (isset($id)) ? "
+        <!-- Yandex.Metrika counter -->
+        <script>
+            (function (d, w, c) {
+                (w[c] = w[c] || []).push(function() {
+                    try {
+                        w.yaCounter{$id} = new Ya.Metrika({
+                            id:{$id},
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true
+                        });
+                    } catch(e) { }
+                });        
+                let n = d.getElementsByTagName('script')[0],
+                    s = d.createElement('script'),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                s.async = true;
+                s.src = 'https://mc.yandex.ru/metrika/watch.js';       
+                if (w.opera == '[object Opera]') {
+                    d.addEventListener('DOMContentLoaded', f, false);
+                } else { f(); }
+            })(document, window, 'yandex_metrika_callbacks');
+        </script>
+        <!-- /Yandex.Metrika counter -->
+    " : '';
+}
